a628152a5d8ade11e5223d00adee132a09946b49
srtipu5 <srtipu5@gmail.com>
2021-07-09 04:23:30 +0600

    first commit

---
+++ Linear_Search/Linear_Search/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Linear_Search
+{
+    class Program
+    {
+        public static string Linear_Search(List<int>arr,int searchValue)
+        {
+            for(int i = 0;i<arr.Count;i++)
+            {
+                if(arr[i] ==searchValue)
+                {
+                    return $"Search value : {searchValue} Position : {i+1}.";
+                }
+            }
+            return $"Search value : {searchValue} not found.";
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Search Value : ");
+            List<int> arr = new List<int>() {23,2,45,1,100,7 };
+            int searchValue = Convert.ToInt32(Console.ReadLine());
+            string result = Linear_Search(arr,searchValue);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
